22946f40640d6ff92e92d8f01b450b5f0122fc89
NVisEdu <xdustedu@gmail.com>
2025-05-23 20:02:32 +0300

    main.py + App.py refactoring

--- app/core/App.py
+++ app/core/App.py
@@ -1,9 +1,32 @@
 import logging
-
 from flask import Flask
+from flask_cors import CORS
 from flask_restx import Api
+from dotenv import load_dotenv
+import os
 
 
-app = Flask(__name__)
+app: Flask = None
+
+
+load_dotenv()
+
+
 api = Api()
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="\033[33m%(message)s\033[0m")
+
+
+def create_app():
+    newapp = Flask(__name__)
+
+    newapp.secret_key = os.getenv("FLASK_SECRET_KEY", "default_secret_key")
+    newapp.config["SESSION_TYPE"] = os.getenv("FLASK_SESSION_TYPE", "filesystem")
+    debug = os.getenv("FLASK_DEBUG", "False").lower() in ("true", "1")
+
+    CORS(newapp, supports_credentials=True)
+
+    api.init_app(newapp)
+    logger.debug("API initialized")
+
+    return newapp
--- main.py
+++ main.py
@@ -1,43 +1,28 @@
-import logging
+import os
 
-from app.core.App import app, api, logger
+from dotenv import load_dotenv
+
+from app.core import App
+from app.core.App import logger
 from app.controllers import namespaces
 from app.database.db_init import Database
-from flask_cors import CORS
 
 
-def configure_app():
-    app.secret_key = "very_secret_key_bro_trust_me_its_secure_af_i_bet_ur_mom"
-    app.config["SESSION_TYPE"] = "SQLAlchemy"
-    CORS(app, supports_credentials=True)
-
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="\033[33m%(message)s\033[0m"
-    )
-
-
-def configure_api():
-    api.init_app(app)
-    logger.debug(f"Flask-RestX API initialized: {api}")
-    for ns in namespaces:
-        api.add_namespace(ns)
-        logger.debug(f"Namespace added: \"{ns.name}\"")
-
-
-def initialize_extensions():
-    Database()
+load_dotenv()
 
 
 def main():
-    configure_app()
-    configure_api()
-    initialize_extensions()
+    App.app = App.create_app()
+    for ns in namespaces:
+        App.api.add_namespace(ns)
+        logger.debug(f"Namespace added: \"{ns.name}\"")
+
+    Database()
 
     logger.debug("App is starting...")
-    app.run(
-        host="193.201.15.217:5050",  # Avoid including port in host
-        port=5050,
+    App.app.run(
+        host=os.getenv("FLASK_HOST", "0.0.0.1"),
+        port=os.getenv("FLASK_PORT", 5000),
         debug=True,
         use_reloader=False
     )
